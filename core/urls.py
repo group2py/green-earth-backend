4e7341caef2e4a4632c5db297a016099d1aaf1e7
Pedro Lucas <suport.pedrodev@gmail.com>
2023-08-02 19:36:53 -0300

    general

--- core/urls.py
+++ core/urls.py
@@ -3,15 +3,12 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
-from home.apis import UserViewSet
 
-router = routers.DefaultRouter()
-router.register('users', UserViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
-] + router.urls
+]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
