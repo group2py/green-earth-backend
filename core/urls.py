039d04c1da95702caa98bfe42aea4ea17eaf5f6b
Pedro Lucas <suport.pedrodev@gmail.com>
2023-08-05 13:35:38 -0300

    routers api

--- core/urls.py
+++ core/urls.py
@@ -1,13 +1,39 @@
-from django.conf import settings
-from django.conf.urls.static import static
-from django.contrib import admin
-from django.urls import path, include
+# IMPORTS API APPS
+from blog import api as api_blog
+from volunteers import api as api_volunteers
+from contributors import api as api_contributors
+from authentication import api as api_authentication
+
+# IMPORT DJANGO REST FRAMEWORK
 from rest_framework import routers
 
+# IMPORTS DJANGO
+from django.conf import settings
+from django.contrib import admin
+from django.urls import path, include
+from django.conf.urls.static import static
+
+# Registared routers
+router = routers.DefaultRouter()
+router.register(r'users', api_authentication.RegisterUser)
+router.register(r'crime', api_blog.CreateCrimeDenunciations)
+router.register(r'list_volunteers', api_volunteers.CreateVolunteers)
+router.register(r'list_contributors', api_contributors.CreateContributors)
+router.register(r'list_contributors_history', api_contributors.CreateContributionHistory)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+
+    # Routers api
+    path('', include(router.urls)),
+    path('get_users/', api_authentication.ListUser.as_view()),
+    path('get_blog/', api_blog.CreateCrimeDenunciations.as_view(actions={'get': 'list'})),
+    path('get_volunteers/', api_volunteers.CreateVolunteers.as_view(actions={'get': 'list'})),
+    path('get_contributors/', api_contributors.CreateContributors.as_view(actions={'get': 'list'})),
+    path('get_contributors_history/', api_contributors.CreateContributionHistory.as_view(actions={'get': 'list'})),
+
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    
 ]
 
 if settings.DEBUG:
