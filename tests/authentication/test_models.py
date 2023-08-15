b56bf5523c258bd1ec51180536ec0652b6007b3c
Vinicius Ranchuka <v.ranchuka@gmail.com>
2023-08-15 18:09:56 -0300

    faker_added

--- tests/authentication/test_models.py
+++ tests/authentication/test_models.py
@@ -1,8 +1,8 @@
 import sys
 import os
 #adding path to parent directory
-# parent_path = os.path.join(os.path.abspath('__path__'), '..','..')
-# sys.path.append(parent_path)
+parent_path = os.path.join(os.path.abspath('__path__'), '..','..')
+sys.path.append(parent_path)
 
 from core import settings
 from django.core.files.uploadedfile import SimpleUploadedFile
@@ -10,30 +10,12 @@ from django.core.files.uploadedfile import SimpleUploadedFile
 
 from django.test import Client
 import pytest
-from model_bakery import baker
+
+from authentication.models import Users
+
 
 @pytest.mark.django_db
-def test_cadastra_usuario_entao_usuario_deve_estar_cadastrado():
-    client = Client()    
-    image_path = os.path.join(settings.BASE_DIR, "static","authentication",  'img', 'profile.png')
-    data_image = SimpleUploadedFile(name='livros.png', content=open(image_path, 'rb').read(),content_type='image/png')
-    data =  {
-        "username": "A",
-        "email": "A@A.COM",
-        "image": data_image,
-        "phone": 998912819,
-        "password": "Aa12aZ3352345&@%",
-        "is_superuser": False,
-        "first_name": "A",
-        "last_name": "Aa",
-        "is_staff": False,
-        "is_active": True,
-    }
-    # data = baker.make("authentication.Users")
-    data['password'] = "Aa12aZ3352345&@%"
-    # data
-    user = client.post('/auth/register/', data=data)
-    print(user.json())
-    database_client = client.get('/auth/users/1/get/')
-    # print(database_client.json()['id'])
-    assert database_client.json()['id']
+def test_register_user_then_user_must_be_registered(client,user_data):
+    client.post('/auth/register/', data=user_data)
+    database_client = Users.objects.filter(email=user_data['email'])
+    assert database_client
--- tests/authentication/test_view.py
+++ tests/authentication/test_view.py
@@ -3,43 +3,17 @@ import os
 
 from core import settings
 from django.core.files.uploadedfile import SimpleUploadedFile
-from django.test import Client
 import pytest
-from model_bakery import baker
-
-
 
 
 @pytest.mark.django_db
-def test_cadastra_usuario_entao_deve_retornar_201(usuario):
-    client = Client()    
-    # image_path = os.path.join(settings.BASE_DIR, "static","authentication",  'img', 'profile.png')
-    # data_image = SimpleUploadedFile(name='livros.png', content=open(image_path, 'rb').read(),content_type='image/png')
-    # data =  {
-    #     "username": "A",
-    #     "email": "A@A.COM",
-    #     "image": data_image,
-    #     "phone": 998912819,
-    #     "password": "Aa12aZ3352345&@%",
-    #     "is_superuser": False,
-    #     "first_name": "A",
-    #     "last_name": "Aa",
-    #     "is_staff": False,
-    #     "is_active": True,
-    # }
-
-    data = usuario
-
-    # Enviando a requisição com a imagem
-
-    response = client.post( '/auth/register/', data=data)
-
+def test_cadastra_usuario_entao_deve_retornar_201(user_data, client):
+    response = client.post( '/auth/register/', data=user_data)
     assert response.status_code == 201
 
 @pytest.mark.skip(reason="Melhorar o sistema de validação de usuário")
 @pytest.mark.django_db
-def test_cadastra_usuario_invalido_entao_deve_retornar_400():
-    client = Client()    
+def test_cadastra_usuario_invalido_entao_deve_retornar_400(client):
     image_path = os.path.join(settings.BASE_DIR, "static","authentication",  'img', 'profile.png')
     data_image = SimpleUploadedFile(name='livros.png', content=open(image_path, 'rb').read(),content_type='image/png')
     data =  {
@@ -48,15 +22,10 @@ def test_cadastra_usuario_invalido_entao_deve_retornar_400():
         "image": data_image,
         "phone": 998912819,
         "password": "Aa12aZ3352345&@%",
-        "is_superuser": False,
         "first_name": "A",
         "last_name": "Aa",
-        "is_staff": False,
-        "is_active": True,
+
     }
-
-
     response = client.post( '/auth/register/', data=data)
-
     assert response.status_code == 400
-    
+
